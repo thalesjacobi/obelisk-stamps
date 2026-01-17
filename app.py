1f5dad3c16c828390d01489ead3bf4677fb37568
thalesjacobi <thalesjacobi@gmail.com>
2026-01-17 21:33:58 +0000

    Fix image tag mismatch in Cloud Run deploy

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ app = Flask(__name__)
 app.secret_key = "supersecretkey"  # Required for flashing messages
 
 # Flask-Mail Setup
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'thalesjacobi@gmail.com'
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
